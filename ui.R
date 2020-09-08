1c146be50a63bf34c5d4852e192bbbd83b4dba2a
ntthung <ntthung@gmail.com>
2020-09-08 15:23:48 +0800

    Add footer.

--- ui.R
+++ ui.R
@@ -18,7 +18,15 @@ fluidPage(
 
     # Show a plot of the selected year
     mainPanel(
-      plotOutput('madaPlot', width = 1000, height = 750)
+      plotOutput('madaPlot', width = 1000, height = 750),
+      HTML('
+        <div style="position: relative; bottom: 0; left: 0; width: 100%; text-align: right;">
+          <br>
+          <p> This is a prototype, version 0.0.0.9000. </p>
+          <p> Author: Nguyen Tan Thai Hung </p>
+          <a href="https://github.com/ntthung">GitHub</a>
+        </div>'
+      )
     )
   )
 )
